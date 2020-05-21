6fcf5dac0480a1454aec23a2ed9ff12e3deb6234
Sepp1324 <stoegerer.sebastian@aon.at>
2020-05-21 23:37:37 +0200

    Tutorial04: Decision-Making (if, elif, else)

--- tut04.py
+++ tut04.py
@@ -1,1 +1,10 @@
 #!usr/bin/env python
+
+_age = input('Input your age: ')
+
+if int(_age) >= 30:
+    print('You are old!')
+elif int(_age) == 20:
+    print('Nice. You are a handsome boy!')
+else:
+    print('You are young!')
