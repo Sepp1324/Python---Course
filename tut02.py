181b96649843717b424e01fae9fc6762b00437ee
Sepp1324 <stoegerer.sebastian@aon.at>
2020-05-21 21:34:05 +0200

    Tutorial02: Expanded Datatypes and introduced to Operators

---
+++ tut02.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+print('Pick a number:')
+_num1 = input()
+
+print('Pick another number:')
+_num2 = input()
+
+_sum = int(_num1) + int(_num2)
+
+print('Sum of ' + str(_num1) + ' and ' + str(_num2) + ' is ' + str(_sum))
