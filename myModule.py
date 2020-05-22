0c95b3b97b8d01f25a657b20a93fcc9e57493ba2
Sepp1324 <stoegerer.sebastian@aon.at>
2020-05-22 11:06:25 +0200

    Tutorial016: Introduced to Modular Programming

---
+++ myModule.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def myFunc(x):
+    return x + 5
+
+def mySecondFunc(x):
+    return x - 5
---
+++ tut016.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import math
+import myModule
+
+print(math.degrees(math.pi))
+print(math.radians(180))
+print(myModule.myFunc(5))
+print(myModule.mySecondFunc(10))
